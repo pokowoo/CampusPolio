cbec67ea2653b86c61b916f55dea5eeeb2db87cb
pokowoo <djldwl123@gmail.com>
2026-05-17 14:12:09 +0900

    시작 캠퍼스폴리오

---
+++ src/main/java/com/campuspolio/CampusPolioApplication.java
@@ -0,0 +1,12 @@
+package com.campuspolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPolioApplication.class, args);
+    }
+}
