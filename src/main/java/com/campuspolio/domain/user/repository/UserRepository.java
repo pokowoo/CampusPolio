9cac533c60d726a8c00810b199b0ee46b0145cce
Minsung0301 <chohein3@naver.com>
2026-05-17 18:58:15 +0900

    feat: UserRepository 조회 메서드 추가

---
+++ src/main/java/com/campuspolio/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.campuspolio.domain.user.repository;
+
+import com.campuspolio.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByGoogleId(String googleId);
+
+    boolean existsByEmail(String email);
+}
