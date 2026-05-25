4afdbd1ab7a2d9d0ce499fcf5783d0c96258a6da
pokowoo <djldwl123@gmail.com>
2026-05-25 18:02:26 +0900

    https api변경

--- src/main/java/com/campuspolio/global/config/SecurityConfig.java
+++ src/main/java/com/campuspolio/global/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                 "http://localhost:5500",
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:5500",
-                "https://campus-polio.vercel.app/",
+                "https://campus-polio.vercel.app",
                 "null"
         ));
 
