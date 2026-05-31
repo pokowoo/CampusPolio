f37f6aea40a2e5be6d67a8033340874eb13ccd11
Minsung0301 <chohein3@naver.com>
2026-05-31 15:26:28 +0900

    chore: swagger 서버주소설정

--- src/main/java/com/campuspolio/global/config/SwaggerConfig.java
+++ src/main/java/com/campuspolio/global/config/SwaggerConfig.java
@@ -2,15 +2,23 @@ package com.campuspolio.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI campusPolioOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://api.campuspolio.cloud");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
                         .title("CampusPolio API")
                         .description("CampusPolio 백엔드 API 명세서")
