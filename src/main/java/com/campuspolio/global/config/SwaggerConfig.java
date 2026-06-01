d1d9f00d20098a07034859664834b7be302e51a4
pokowoo <djldwl123@gmail.com>
2026-06-01 17:13:53 +0900

    fix swagger https server

--- src/main/java/com/campuspolio/global/config/SwaggerConfig.java
+++ src/main/java/com/campuspolio/global/config/SwaggerConfig.java
@@ -2,18 +2,28 @@ package com.campuspolio.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI campusPolioOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://api.campuspolio.cloud");
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("CampusPolio API")
-                        .description("CampusPolio 백엔드 API 명세서")
-                        .version("v1.0.0"));
+                .servers(List.of(server))
+                .info(
+                        new Info()
+                                .title("CampusPolio API")
+                                .description("CampusPolio 백엔드 API 명세서")
+                                .version("v1.0.0")
+                );
     }
 }
