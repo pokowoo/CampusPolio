fda5969d23348df5d993ba97157760139fb68bd1
pokowoo <djldwl123@gmail.com>
2026-06-01 22:33:33 +0900

    스웨거2 변경

--- src/main/java/com/campuspolio/global/config/SwaggerConfig.java
+++ src/main/java/com/campuspolio/global/config/SwaggerConfig.java
@@ -11,15 +11,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI campusPolioOpenAPI() {
+
         return new OpenAPI()
+
                 .addServersItem(
                         new Server()
                                 .url("https://api.campuspolio.cloud")
+                                .description("Production Server")
                 )
+
                 .info(
                         new Info()
                                 .title("CampusPolio API")
-                                .description("CampusPolio API")
+                                .description("CampusPolio 백엔드 API 명세서")
                                 .version("v1.0.0")
                 );
     }
