7b4479e34403a8ac5ba993cf335730383c1fe3a6
pokowoo <djldwl123@gmail.com>
2026-06-01 23:08:30 +0900

    스웨거 테스트4  변경

--- src/main/java/com/campuspolio/global/config/SwaggerConfig.java
+++ src/main/java/com/campuspolio/global/config/SwaggerConfig.java
@@ -6,24 +6,31 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI campusPolioOpenAPI() {
-        return new OpenAPI()
 
-                .addServersItem(
+        OpenAPI openAPI = new OpenAPI();
+
+        openAPI.setServers(
+                List.of(
                         new Server()
                                 .url("https://api.campuspolio.cloud")
                                 .description("Production Server")
                 )
+        );
 
-                .info(
-                        new Info()
-                                .title("CampusPolio API")
-                                .description("CampusPolio 백엔드 API 명세서")
-                                .version("v1.0.0")
-                );
+        openAPI.setInfo(
+                new Info()
+                        .title("CampusPolio API")
+                        .description("CampusPolio 백엔드 API 명세서")
+                        .version("v1.0.0")
+        );
+
+        return openAPI;
     }
 }
